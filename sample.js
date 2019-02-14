d2bb6eb9f71e7cc3cf5d401494ec8d77ccbdee45
nkiyo <nkiyo@hoge.com>
2019-02-15 08:14:08 +0900

    up

--- sample.js
+++ sample.js
@@ -1,3 +1,4 @@
+// Promiseを返すサンプル関数
 async function sample(value, toSec) {
     console.log(`sample(${value}) start`);
     return new Promise((resolve, reject) => {
@@ -9,16 +10,6 @@ async function sample(value, toSec) {
             resolve(value * 2);
         }, toSec * 1000);
     });
-    //throw new Error('reject!');
-    //return 'resolve!';
-}
-
-function func() {
-    sample().then(value => {
-        console.log(`then ${value}`);
-    }).catch(err => {
-       console.log(`catch ${err}`);
-    });
 }
 
 // 直列で非同期処理を実行
@@ -28,7 +19,7 @@ async function serialFunc() {
         const res1 = await sample(5, 5);
         console.log(`...`);
         const res2 = await sample(3, 3);
-        const res444 = await sample(444, 4);
+        //const res444 = await sample(444, 4);
         console.log(`serialFunc() end`);
         return res1 + res2;
     } catch(e) {
@@ -41,13 +32,47 @@ async function serialFunc() {
 
 // 並列で非同期処理を実行
 async function parallelFunc() {
+    const [a, b, c] = await Promise.all([
+        sample(3, 3),
+        sample(2, 2),
+        sample(1, 1),
+        //sample(444, 1),
+    ]);
+
+    return [a, b, c];
 }
 
-serialFunc().
-then(res => {
-    console.log(`res is ${res}.`);
-})
-.catch(err => {
+async function parallelFunc2() {
+    const a = await Promise.race([
+        sample(3, 3),
+        sample(2, 2),
+        sample(1, 1),
+        //sample(444, 1),
+    ]);
+
+    return a;
+}
+
+//serialFunc().
+//then(res => {
+//    console.log(`res is ${res}.`);
+//})
+//.catch(err => {
+//    console.error(err.stack);
+//});
+
+//parallelFunc().
+//then(([a, b, c]) => {
+//    console.log(`res is ${a + b + c}`);
+//}).
+//catch(err => {
+//    console.error(err.stack);
+//});
+
+parallelFunc2().
+then((a) => {
+    console.log(`res is ${a}`);
+}).
+catch(err => {
     console.error(err.stack);
 });
-
